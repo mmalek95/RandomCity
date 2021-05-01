d52d119459ab9df4dd1d7e586ceaffff9f173885
Maciej Małek <maciek.malek.22@gmail.com>
2021-05-01 22:32:02 +0200

    add random city provider implementation

--- data/src/main/java/com/malek/data/models/RandomCityDto.kt
+++ data/src/main/java/com/malek/data/models/RandomCityDto.kt
@@ -1,3 +1,3 @@
 package com.malek.data.models
 
-data class RandomCityDto(val name: String)
+data class RandomCityDto(val name: String?, val color: String?)
--- data/src/main/java/com/malek/data/providers/impl/DefaultRandomCityProvider.kt
+++ data/src/main/java/com/malek/data/providers/impl/DefaultRandomCityProvider.kt
@@ -2,10 +2,30 @@ package com.malek.data.providers.impl
 
 import com.malek.data.models.RandomCityDto
 import com.malek.data.providers.RandomCityProvider
+import kotlin.random.Random
 
 class DefaultRandomCityProvider: RandomCityProvider {
 
+    private val cities = listOf("Gdańsk", "Warszawa", "Poznań")
+    private val colors = listOf("Yellow", "Green", "Blue")
+
     override fun provideRandomCity(): RandomCityDto {
-        return RandomCityDto("test random city")
+        return RandomCityDto(getRandomCityName(), getRandomColor())
+    }
+
+    private fun getRandomCityName(): String? {
+        return cities.getOrNull(getRandomNumber(cities.size))
+    }
+
+    private fun getRandomColor(): String? {
+        return colors.getOrNull(getRandomNumber(colors.size))
+    }
+
+    private fun getRandomNumber(until: Int): Int {
+        return try {
+            Random.nextInt(0, until)
+        } catch (exception: IllegalArgumentException) {
+            0
+        }
     }
 }
