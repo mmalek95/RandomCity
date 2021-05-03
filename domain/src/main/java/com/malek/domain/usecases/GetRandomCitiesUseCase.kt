7665fa5f4a3f457a82cb0e9b606c7c1abf4025d8
Maciej Małek <maciek.malek.22@gmail.com>
2021-05-03 13:27:32 +0200

    fix city sorting

--- domain/src/main/java/com/malek/domain/usecases/GetRandomCitiesUseCase.kt
+++ domain/src/main/java/com/malek/domain/usecases/GetRandomCitiesUseCase.kt
@@ -3,14 +3,14 @@ package com.malek.domain.usecases
 import com.malek.domain.models.RandomCity
 import com.malek.domain.repositories.RandomCityRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.flow.map
 
 class GetRandomCitiesUseCase constructor(private val randomCityRepository: RandomCityRepository) {
 
     fun getRandomCities(): Flow<List<RandomCity?>> {
         return randomCityRepository
             .getRandomCities()
-            .onEach(this::sortByName)
+            .map(this::sortByName)
     }
 
     private fun sortByName(randomCities: List<RandomCity?>): List<RandomCity?> {
