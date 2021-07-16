b95d196a079c2a8d77cde55ab403984f22a893e9
Maciej Małek <maciej.malek@netguru.com>
2021-07-16 10:52:08 +0200

    fix tests

---
+++ app/src/androidTest/java/com/malek/randomcityapp/ui/splash/activities/SplashActivityTest.kt
@@ -0,0 +1,40 @@
+package com.malek.randomcityapp.ui.splash.activities
+
+
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.runner.AndroidJUnit4
+import com.malek.randomcityapp.R
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@LargeTest
+@RunWith(AndroidJUnit4::class)
+class SplashActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashActivity::class.java)
+
+    @Test
+    fun splashActivityTest() {
+        val textView = onView(
+            allOf(
+                withText("RandomCityApp"),
+                withParent(
+                    allOf(
+                        withId(R.id.action_bar),
+                        withParent(withId(R.id.action_bar_container))
+                    )
+                ),
+                isDisplayed()
+            )
+        )
+        textView.check(matches(withText("RandomCityApp")))
+        }
+    }
